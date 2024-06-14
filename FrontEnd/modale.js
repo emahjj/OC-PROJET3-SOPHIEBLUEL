b8563b036365ed3b4dcff1516eb77589c8e4d866
Emma Huj <emmahuj@live.fr>
2024-06-14 10:37:11 +0200

    Delete works button

--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -10,8 +10,6 @@ document.addEventListener("DOMContentLoaded", async () => {
             authLink.innerHTML = "logout";	
             
   		}
-        
-
 
         // Sélectionner l'élément où ajouter le bouton d'édition
         const headersEdition = document.querySelector("#portfolio h2");
@@ -66,17 +64,36 @@ document.addEventListener("DOMContentLoaded", async () => {
             for (let i = 0; i < works.length; i++) {
                 const portfolio = works[i];
                 
+                // Créer le conteneur d'image
+                const imageContainer = document.createElement("div");
+                imageContainer.classList.add("image-container");
+
+                // Créer le bouton "delete"
+                const deleteButton = document.createElement("button");
+                deleteButton.classList.add("delete-button");
+                deleteButton.textContent = "Delete";
+                deleteButton.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="5" height="7" viewBox="0 0 9 11" fill="none">
+                <path d="M2.71607 0.35558C2.82455 0.136607 3.04754 0 3.29063 0H5.70938C5.95246 0 6.17545 0.136607 6.28393 0.35558L6.42857 0.642857H8.35714C8.71272 0.642857 9 0.930134 9 1.28571C9 1.64129 8.71272 1.92857 8.35714 1.92857H0.642857C0.287277 1.92857 0 1.64129 0 1.28571C0 0.930134 0.287277 0.642857 0.642857 0.642857H2.57143L2.71607 0.35558ZM0.642857 2.57143H8.35714V9C8.35714 9.70915 7.78058 10.2857 7.07143 10.2857H1.92857C1.21942 10.2857 0.642857 9.70915 0.642857 9V2.57143ZM2.57143 3.85714C2.39464 3.85714 2.25 4.00179 2.25 4.17857V8.67857C2.25 8.85536 2.39464 9 2.57143 9C2.74821 9 2.89286 8.85536 2.89286 8.67857V4.17857C2.89286 4.00179 2.74821 3.85714 2.57143 3.85714ZM4.5 3.85714C4.32321 3.85714 4.17857 4.00179 4.17857 4.17857V8.67857C4.17857 8.85536 4.32321 9 4.5 9C4.67679 9 4.82143 8.85536 4.82143 8.67857V4.17857C4.82143 4.00179 4.67679 3.85714 4.5 3.85714ZM6.42857 3.85714C6.25179 3.85714 6.10714 4.00179 6.10714 4.17857V8.67857C6.10714 8.85536 6.25179 9 6.42857 9C6.60536 9 6.75 8.85536 6.75 8.67857V4.17857C6.75 4.00179 6.60536 3.85714 6.42857 3.85714Z" fill="white"/>
+                </svg>`;
+
+                // Créer l'image
                 const imageElementmodal = document.createElement("img");
                 imageElementmodal.src = portfolio.imageUrl;
                 imageElementmodal.classList.add("img-modal");
                 imageElementmodal.id = portfolio.id;
 
                 // Ajout des éléments au DOM
-                sectionModal.appendChild(imageElementmodal);
+                imageContainer.appendChild(deleteButton);
+                imageContainer.appendChild(imageElementmodal);
+                sectionModal.appendChild(imageContainer);
 
                 // Au click, Récupérer l'id du projet 
-                imageElementmodal.addEventListener("click", function (event) {
-                    const clickedImageId = event.target.id;
+                deleteButton.addEventListener("click", function (event) {
+
+                    const imageElementmodal = document.querySelector(".img-modal");
+
+                    clickedImageId = imageElementmodal.id;
                     console.log(clickedImageId); 
 
                     // Construire l'URL de l'API avec l'ID cliqué
@@ -89,8 +106,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                         'Authorization': `Bearer ${token}` // Ajouter le token d'authentification
                         },
                         body: JSON.stringify({ id: clickedImageId }) // Convert id to a JSON string
-                    })     
-                    console.log(workdeleted)      
+                    })          
                 })
              }
         } 
